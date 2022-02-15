1cfc47dd6eb8aaa57dbea816b776689eb4bfb315
sunxiaochun-code <13050571762@163.com>
2022-02-15 08:38:17 +0800

    sheji

--- src/main/java/com/xuan/design/DesignPatternApplication.java
+++ src/main/java/com/xuan/design/DesignPatternApplication.java
@@ -3,6 +3,9 @@ package com.xuan.design;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 设计模式
+ */
 @SpringBootApplication
 public class DesignPatternApplication {
 
