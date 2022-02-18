5b9f2d7300fbb1c7320fdf2c363324e2dc874c68
zhangshixuan <493140635@qq.com>
2022-02-18 16:03:29 +0800

    中介者模式

---
+++ src/main/java/com/xuan/design/behavior_type/observer/jdk/ObserverDemo.java
@@ -0,0 +1,11 @@
+package com.xuan.design.behavior_type.observer.jdk;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverDemo {
+
+    public static void main(String[] args) {
+//        Observable
+    }
+}
